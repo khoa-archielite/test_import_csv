b89b75142766d0515e6ed847c89079b9e651e129
khoa-archielite <khoa@archielite.com>
2022-12-05 02:37:12 +0700

    update: import csv logic

--- resources/views/frontend/csv/import.blade.php
+++ resources/views/frontend/csv/import.blade.php
@@ -1,14 +1,22 @@
 @extends('frontend.layouts.base')
 @section('content')
-
-    <form action="{{ route('csv.store') }}" method="post" enctype="multipart/form-data">
-        @csrf
-        <div class="mb-3">
-            <label for="import_csv" class="form-label">{{ __('Import user from csv') }}</label>
-            <input type="file" class="form-control" name="csv_file" id="import_csv">
-            <div id="csvHelpText" class="form-text">{{ __('Import file csv') }}</div>
-
-            <button type="submit" class="btn btn-primary">{{ __('Import') }}</button>
-        </div>
-    </form>
+    <div class="container">
+        <h1 class="mt-3 text-center">{{ __('Start Import file') }}</h1>
+        <form action="{{ route('csv.store') }}" class="mt-3" method="post" enctype="multipart/form-data">
+            @csrf
+            <div class="mb-3">
+                <label for="import_csv" class="form-label">{{ __('Import user from csv') }}</label>
+                <input type="file" class="form-control" name="csv_file" id="import_csv">
+                <div id="csvHelpText" class="form-text">{{ __('Import file csv') }}</div>
+                @error('csv_file')
+                <span class="d-block invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+                <div class="mt-5">
+                    <button type="submit" class="btn btn-primary">{{ __('Import') }}</button>
+                </div>
+            </div>
+        </form>
+    </div>
 @endsection
